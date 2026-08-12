2399fb6bbaddcda7c4efcd70d3af7fd8c1127f1e
Luzolo mampassi <luzolomampassi8@gmail.com>
2026-08-12 11:18:44 +0100

    A

--- Projetos/Web sites/Landings pages/Dashbord anality/main.js
+++ Projetos/Web sites/Landings pages/Dashbord anality/main.js
@@ -1,10 +1,11 @@
 const btn_menu = document.getElementById('btn-menu')
 const menu = document.getElementById('menu')
-
-console.log(btn_menu)
-console.log(menu)
+const btn_dark = document.getElementById('btn-dark')
 
 btn_menu.addEventListener('click', () => {
     console.log("clicou")
     menu.classList.toggle('ativo')
+})
+btn_dark.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme')
 })
